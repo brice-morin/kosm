681e62ac8f7314b2a78c95e5709af5815ec8f345
brice-morin <brice.morin1@gmail.com>
2014-01-28 12:30:40 +0100

    workaround for JS, see http://youtrack.jetbrains.com/issue/KT-4479

--- src/main/java/org/sintef/kosm/Transitions.kt
+++ src/main/java/org/sintef/kosm/Transitions.kt
@@ -1,7 +1,7 @@
 package org.sintef.kosm
 
-abstract class Handler(val action : HandlerAction = NullHandlerAction, val event : EventType) : HandlerAction by action {
-
+abstract class Handler(val action : HandlerAction = NullHandlerAction, val event : EventType) : HandlerAction /*by action*/ {
+    override fun execute() = action.execute()
 }
 
 open class Transition(val source : StateT, val target : StateT, action : HandlerAction = NullHandlerAction, event : EventType) : Handler(action, event) {
